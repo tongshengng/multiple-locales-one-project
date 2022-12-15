f0b2a1f7af719b2ef82e0127b160fe56c8680dc9
Tong Sheng Ng <tongsheng.ng@stashaway.com>
2022-12-15 22:02:30 +0800

    test

---
+++ pages/[id].tsx
@@ -0,0 +1,35 @@
+import type {
+  GetStaticPathsContext,
+  GetStaticPropsContext,
+  NextPage,
+} from "next";
+import { useRouter } from "next/router";
+import config from "../next.config";
+
+export async function getStaticPaths(ctx: GetStaticPathsContext) {
+  return {
+    paths: [],
+    fallback: "blocking",
+  };
+}
+
+export async function getStaticProps(ctx: GetStaticPropsContext) {
+  return {
+    props: {},
+  };
+}
+
+const Home: NextPage = () => {
+  const router = useRouter();
+
+  const regionLocales = config.i18n?.domains?.find(
+    (domain) => router.locale && domain.locales?.includes(router.locale)
+  )?.locales;
+
+  console.log(router);
+  console.log(regionLocales);
+
+  return <div></div>;
+};
+
+export default Home;
