a225dfe6476760023fb443830fbc311338b563cd
Tong Sheng Ng <tongsheng.ng@stashaway.com>
2022-12-16 12:31:13 +0800

    test

---
+++ middleware.ts
@@ -0,0 +1,38 @@
+import { NextResponse } from "next/server";
+
+import type { NextRequest } from "next/server";
+import { getDomainLocalesByHostname } from "./utils/getDomainLocalesByHostname";
+import { getLocaleByURL } from "./utils/getLocaleByURL";
+
+export async function middleware(req: NextRequest) {
+  if (req.url) {
+    return;
+  }
+
+  if (
+    req.url.includes("/_next/") ||
+    req.url.includes("/api/") ||
+    req.url.includes("favicon.ico")
+  ) {
+    return;
+  }
+
+  if (process.env.NODE_ENV === "development") {
+    return;
+  }
+
+  const locale = getLocaleByURL(req.nextUrl);
+
+  if (!locale) {
+    return;
+  }
+
+  const domainLocales = getDomainLocalesByHostname(req.nextUrl.hostname);
+
+  if (!domainLocales?.includes(locale)) {
+    const url = req.nextUrl.clone();
+    url.pathname = "/404";
+
+    return NextResponse.rewrite(url);
+  }
+}
--- next.config.js
+++ next.config.js
@@ -1,5 +1,6 @@
-/** @type {import('next').NextConfig} */
+const domains = require("./domains");
 
+/** @type {import('next').NextConfig} */
 module.exports = {
   reactStrictMode: true,
   swcMinify: true,
@@ -7,32 +8,6 @@ module.exports = {
     locales: ["en-SG", "en-MY", "en-AE", "en-TH", "th-TH", "en-HK", "zh-HK"],
     defaultLocale: "en-SG",
     localeDetection: false,
-    domains: [
-      {
-        domain: "stashsg.vercel.app",
-        defaultLocale: "en-SG",
-        locales: ["en-SG"],
-      },
-      {
-        domain: "stashmy.vercel.app",
-        defaultLocale: "en-MY",
-        locales: ["en-MY"],
-      },
-      {
-        domain: "stashae.vercel.app",
-        defaultLocale: "en-AE",
-        locales: ["en-AE"],
-      },
-      {
-        domain: "stashth.vercel.app",
-        defaultLocale: "en-TH",
-        locales: ["en-TH", "th-TH"],
-      },
-      {
-        domain: "stashhk.vercel.app",
-        defaultLocale: "en-HK",
-        locales: ["en-HK", "zh-HK"],
-      },
-    ],
+    domains,
   },
 };
