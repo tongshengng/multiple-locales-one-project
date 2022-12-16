53ebafd0b2d3556ed80fe178721799d6e745430b
Tong Sheng Ng <tongsheng.ng@stashaway.com>
2022-12-16 12:50:37 +0800

    test

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,4 +1,5 @@
 import type { NextPage } from "next";
+import Link from "next/link";
 import { useRouter } from "next/router";
 import { useMemo } from "react";
 import { i18n } from "../next.config";
@@ -9,7 +10,7 @@ const Home: NextPage = () => {
   const domainLocales = useMemo(
     () =>
       JSON.stringify(
-        i18n?.domains?.find(
+        i18n?.domains?.filter(
           ({ locales }) => router.locale && locales?.includes(router.locale)
         ),
         null,
@@ -18,7 +19,19 @@ const Home: NextPage = () => {
     [router.locale]
   );
 
-  return <pre>{domainLocales}</pre>;
+  return (
+    <div style={{ display: "flex", flexDirection: "column", gap: "1rem" }}>
+      <h4></h4>
+      <pre>{domainLocales}</pre>
+      <div style={{ display: "flex", gap: "1rem" }}>
+        {i18n?.locales.map((locale) => (
+          <Link key={locale} href="/" locale={locale}>
+            <button>{locale}</button>
+          </Link>
+        ))}
+      </div>
+    </div>
+  );
 };
 
 export default Home;
